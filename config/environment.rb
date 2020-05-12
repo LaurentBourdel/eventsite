d24b1ec2aae29e52b1b6e317dcfdb1ef2ba13847
LaurentBourdel <solartech971@gmail.com>
2020-05-12 16:55:46 -0400

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,12 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['solartech971'],
-    :password => ENV['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'],
-    :domain => 'https://www.solartech971.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+  ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
