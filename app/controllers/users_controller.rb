8b40de9a53458672ff10f42d6f480d1a4f350488
LaurentBourdel <solartech971@gmail.com>
2020-05-12 14:49:37 -0400

    a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-	#before_action :authenticate_user!
-  #before_action :is_adequate_user?, only: [:show]
+	before_action :authenticate_user!
+  before_action :is_adequate_user?, only: [:show]
 
   def show
     @user = User.find(params['id'])
