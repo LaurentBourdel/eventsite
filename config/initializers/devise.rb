3b234b452e9ecfd3f70993abfa157924b25ed20d
LaurentBourdel <solartech971@gmail.com>
2020-05-12 16:16:37 -0400

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'solartech971@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -290,6 +290,7 @@ Devise.setup do |config|
   # ActiveSupport.on_load(:devise_failure_app) do
   #   include Turbolinks::Controller
   # end
+
 
   # ==> Configuration for :registerable
 
