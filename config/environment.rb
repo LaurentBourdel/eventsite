27371e70039fc4d076192c0c1073912837ddd8b2
LaurentBourdel <solartech971@gmail.com>
2020-05-14 11:22:22 -0400

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 # Load the Rails application.
 require_relative 'application'
+require 'dotenv'# Appelle la gem Dotenv
 
+Dotenv.load('.env')
 # Initialize the Rails application.
 Rails.application.initialize!
 
